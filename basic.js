1ce0a262fb995604e95ac2ff8c6232c5b98ec8ee
Oil Sudarat <sudarat@enres.co>
2021-02-01 20:57:12 +0700

    Add getBase64 from url

--- basic.js
+++ basic.js
@@ -1,3 +1,4 @@
+const axios = require("axios");
 const fs = require("fs");
 const PdfPrinter = require("pdfmake");
 
@@ -10,15 +11,39 @@ const fonts = {
   },
 };
 
-const printer = new PdfPrinter(fonts);
-
-var docDefinition = {
-  content: [
-    "First paragraph",
-    "Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines",
-  ],
+const getBase64 = (url) => {
+  return axios
+    .get(url, {
+      responseType: "arraybuffer",
+    })
+    .then((response) => {
+      const image = Buffer.from(response.data, "binary").toString("base64");
+      return `data:${response.headers[
+        "content-type"
+      ].toLowerCase()};base64,${image}`;
+    });
 };
 
-var pdfDoc = printer.createPdfKitDocument(docDefinition);
-pdfDoc.pipe(fs.createWriteStream("basics.pdf"));
-pdfDoc.end();
+const main = async () => {
+  const url =
+    "https://lh3.googleusercontent.com/ogw/ADGmqu9XN31Kua0O0Ny1dCgxNL4ZoHf1U-ESjuzPzMs1=s32-c-mo";
+
+  const docDefinition = {
+    content: [
+      "First paragraph",
+      "Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines",
+      {
+        image: await getBase64(url),
+      },
+    ],
+  };
+
+  const printer = new PdfPrinter(fonts);
+  const pdfDoc = printer.createPdfKitDocument(docDefinition);
+  pdfDoc.pipe(fs.createWriteStream("image-basics.pdf"));
+  pdfDoc.end();
+};
+
+main();
+
+console.log("Done!!");
